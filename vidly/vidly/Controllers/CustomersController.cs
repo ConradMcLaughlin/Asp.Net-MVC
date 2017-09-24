6a09de114f85b2a6c91230801117f95ee187329a
Conrad <Conrad@CONRAD-PC>
2017-09-24 12:04:24 +0100

    Create action now Save. Customer model updated to Db. Hidden id field.

--- vidly/vidly/Controllers/CustomersController.cs
+++ vidly/vidly/Controllers/CustomersController.cs
@@ -56,9 +56,20 @@ namespace vidly.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.ID == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.ID == customer.ID);
+                customerInDb.Name = customer.Name;
+                customerInDb.DOB = customer.DOB;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
