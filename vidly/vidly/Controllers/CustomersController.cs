d46b5f5f6b55096bb1603e2d597645f3bc30b496
Conrad <Conrad@CONRAD-PC>
2017-10-08 15:00:30 +0100

    Validation on Customer Name field

--- vidly/vidly/Controllers/CustomersController.cs
+++ vidly/vidly/Controllers/CustomersController.cs
@@ -58,6 +58,18 @@ namespace vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            //Validation
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel()
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+            
             if (customer.ID == 0)
             {
                 _context.Customers.Add(customer);
